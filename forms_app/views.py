4664c3e63fe31871e9ed121c68d6f9698d17a6e4
Simone Cantoni <71756284+cantonis@users.noreply.github.com>
2021-03-17 12:54:23 +0100

    L24

--- forms_app/views.py
+++ forms_app/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .forms import FormContatto
 
 # Create your views here.
 
 
 def contatti(request):
-    form = FormContatto()
+    if request.method == "POST":
+        form = FormContatto(request.POST)
+        if form.is_valid():
+            print("Il form è valido!")
+            print("NOME:", form.cleaned_data["nome"])
+            print("COGNOME:", form.cleaned_data["cognome"])
+            print("EMAIL:", form.cleaned_data["email"])
+            print("CONTENUTO:", form.cleaned_data["contenuto"])
+
+            return HttpResponse("<h1>Grazie per averci contattato!</h1>")
+    else:
+        form = FormContatto()
+
     context = {"form": form}
     return render(request, "contatto.html", context)
